916359d8e206e518399b93cefc72dc8832803ed6
Rudger322 <vovannello@gmail.com>
2025-05-10 21:19:03 +0500

    done

--- index.php
+++ index.php
@@ -1,95 +1,35 @@
 <?php
-// задание 1
-
-function func($i){
-    do {
-        echo $i;
-        if($i == 0){
-            echo ' - это ноль';
-        } else
-        if($i % 2 == 0){
-            echo ' - четное число';
-        } else{
-            echo ' - нечетное число';
-        };
-        echo '<br>';
-        $i++;
-    } while ($i<=10);
-};
-func(5);
-// задание 2
-$regions = [
-    "Московская область" => ["Москва", "Зеленоград", "Клин"],
-    "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
-    "Рязанская область" => ["Рязань", "Касимов", "Скопин"]
-];
-
-foreach ($regions as $region => $cities) {
-    echo $region . ":\n";
-    echo implode(", ", $cities) . ".\n";
-    echo '<br>';
-};
-// задание 3
-function func3($word){
-    $translit = [
-        'а' => 'a',
-        'б' => 'b',
-        'в' => 'v',
-        'г' => 'g',
-        'д' => 'd',
-        'е' => 'e',
-        'ё' => 'yo',
-        'ж' => 'zh',
-        'з' => 'z',
-        'и' => 'i',
-        'й' => 'y',
-        'к' => 'k',
-        'л' => 'l',
-        'м' => 'm',
-        'н' => 'n',
-        'о' => 'o',
-        'п' => 'p',
-        'р' => 'r',
-        'с' => 's',
-        'т' => 't',
-        'у' => 'u',
-        'ф' => 'f',
-        'х' => 'kh',
-        'ц' => 'ts',
-        'ч' => 'ch',
-        'ш' => 'sh',
-        'щ' => 'shch',
-        'ъ' => '',
-        'ы' => 'y',
-        'ь' => '',
-        'э' => 'e',
-        'ю' => 'yu',
-        'я' => 'ya'
-    ];
-    $word2 = mb_strtolower($word);
-    $result = strtr($word2, $translit);
-    return $result;
-};
-echo func3('слово помидор');
-// задание 4
-function func4($mass){
-    echo '<ul>';
-    foreach($mass as $key => $value){
-        if(!is_array($value)){
-            echo '<li>';
-            echo $value;
-            echo '</li>';
-            echo '<br>';
-        } else{
-            echo '<li>';
-            echo $key;
-            echo '</li>';
-            func4($value);
-        };
+$files = glob('./assets/*');
+foreach ($files as $file) {
+    if (is_file($file)) {
+        $img = "./assets/" . basename($file);
+        echo '<a href="'.$img.'" target="_blank">';
+        echo '<img src="' . $img . '" alt="Фото" style="max-width: 150px; max-height: 150px;">';
+        echo '</a>';
     }
-    echo '</ul>';
-};
-func4($regions);
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+
+        $uploadDir = 'assets/';
+        $uploadFile = $uploadDir . basename($_FILES['image']['name']);
+        $fileExtension = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
+        $fileSize = $_FILES['image']['size'];
+
+        if ($fileSize <= 5 * 1024 * 1024 && in_array($fileExtension, ['jpg', 'gif', 'png'])) {
+            if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
+                header("Location: " . $_SERVER['PHP_SELF'] . "?success=1");
+                exit();
+            } else {
+                header("Location: " . $_SERVER['PHP_SELF'] . "?error=move");
+                exit();
+            }
+        } else {
+            header("Location: " . $_SERVER['PHP_SELF'] . "?error=upload");
+            exit();
+        }
+    }
+}
 ?>
 
 
@@ -101,6 +41,10 @@ func4($regions);
     <title><?= $title ?></title>
 </head>
 <body>
-    
+    <form action="" method="post" enctype="multipart/form-data">
+        <label>Загрузить изображение (JPG, PNG, GIF, до 5MB):</label><br>
+        <input type="file" name="image" required>
+        <button type="submit">Загрузить</button>
+    </form>
 </body>
 </html>
